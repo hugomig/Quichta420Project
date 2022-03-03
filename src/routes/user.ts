567ed8ed03e3e9867ea5d28a6b8aac3a2884efae
Hugo Mignotte <hugo.mignotte@gmail.com>
2022-03-03 21:00:11 +0100

    update post user + add schema validation

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,16 +2,24 @@ import { FastifyInstance } from 'fastify';
 import { connection } from '../lib/connection';
 import { User } from '../entities/User';
 import { hashPassword } from '../lib/passwordEncryption';
+import * as UserSchema from '../schemas/user.json';
 
 export const userRoutes = async (fastify: FastifyInstance) => {
     fastify.route<{ Body: User }>({
         method: 'POST',
         url: '/',
         schema: {
-            tags: ['User']
+            tags: ['User'],
+            body: UserSchema
         },
         handler: async (req, res) => {
             try {
+                const alreadyExistUsername = await connection.getRepository(User).findOne({ username: req.body.username });
+                if(alreadyExistUsername) return res.status(400).send("Sorry this username is already taken");
+
+                const alreadyExistEmail = await connection.getRepository(User).findOne({ email: req.body.email });
+                if(alreadyExistEmail) return res.status(400).send("Sorry this email is already taken");
+
                 const newUser = req.body;
                 newUser.password = await hashPassword(req.body.password);
                 await connection.getRepository(User).insert(newUser);
