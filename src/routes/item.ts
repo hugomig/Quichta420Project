838443abe5fd632257dda2a3a0bbe74c7ea036c9
Hugo Mignotte <hugo.mignotte@gmail.com>
2022-03-03 20:59:49 +0100

    update post party + add schema validation

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,19 +1,44 @@
 import { FastifyInstance } from 'fastify';
+import { Invitation } from '../entities/Invitation';
+import { Item, ItemType } from '../entities/Item';
 import { Party } from '../entities/Party';
 import { connection } from '../lib/connection';
+import * as ItemSchema from '../schemas/item.json';
+
+interface PostItemBodyType {
+    type?: ItemType,
+    name: string,
+    description?: string,
+    quantity?: number,
+    invitation: string
+}
 
 export const itemRoutes = async (fastify : FastifyInstance) => {
-    fastify.route({
+    fastify.route<{Body: PostItemBodyType}>({
         method: 'POST',
         url: '/',
         schema: {
-            tags: ['Item']
+            tags: ['Item'],
+            body: ItemSchema
         },
         preValidation: async (req, res) => {
             fastify.verifyJwt(req, res);
         },
         handler: async (req, res) => {
-            res.send(200);
+            const newItem = new Item();
+
+            const invitation = await connection.getRepository(Invitation).findOne({ id: req.body.invitation });
+            if(!invitation) return res.status(400).send("Sorry this invitation doesnt exist");
+            newItem.invitation = invitation;
+
+            if(req.body.type) newItem.type = req.body.type;
+            if(req.body.description) newItem.description = req.body.description;
+            if(req.body.quantity) newItem.quantity = req.body.quantity;
+            newItem.name = req.body.name;
+
+            await connection.getRepository(Item).insert(newItem);
+
+            res.status(200).send(newItem.id);
         }
     });
 
