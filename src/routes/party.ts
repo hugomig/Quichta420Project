37b94959a413159eeb0b353af2c881169cc5fb4d
Hugo Mignotte <hugo.mignotte@gmail.com>
2022-03-11 13:55:35 +0100

    user routes delete patch and fix get + party routes delete patch and fix get

--- src/routes/party.ts
+++ src/routes/party.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify';
+import { Invitation } from '../entities/Invitation';
 import { Party } from '../entities/Party';
 import { User } from '../entities/User';
 import { connection } from '../lib/connection';
@@ -16,7 +17,14 @@ export const partyRoutes = async (fastify: FastifyInstance) => {
             fastify.verifyJwt(req, res);
         },
         handler: async (req, res) => {
-            const newParty = req.body;
+            const newParty = new Party();
+            newParty.name = req.body.name;
+            newParty.location = req.body.location;
+            newParty.date = req.body.date;
+            newParty.description = req.body.description;
+            newParty.minimumAge = req.body.minimumAge;
+            newParty.maximumAge = req.body.maximumAge;
+
             const user = await connection.getRepository(User).findOne({id: req.user.id});
             if(!user) return res.status(400).send("Unexpected check your auth token");
 
@@ -31,15 +39,59 @@ export const partyRoutes = async (fastify: FastifyInstance) => {
         method: 'GET',
         url: '/',
         schema: {
-            tags: ['Party']
+            tags: ['Party'],
+            description: "Get all the parties which you are invited at"
         },
         preValidation: async (req, res) => {
             fastify.verifyJwt(req, res);
         },
         handler: async (req, res) => {
-            const test = req.user.id
-            const parties = await connection.getRepository(Party).find();
-            return res.send(test);
+            const user = await connection.getRepository(User).findOne({ id: req.user.id });
+            const invitations = await connection.getRepository(Invitation).find({ user: user });
+
+            const invitedParties: Party[] = []
+            for(const invitation of invitations) {
+                invitedParties.push(invitation.party);
+            }
+
+            const createdParties = await connection.getRepository(Party).find({ creator: user });
+
+            res.status(200).send({ invitedParties: invitedParties, createdParties: createdParties });
+        }
+    });
+
+    fastify.route<{ Params: { username: string }}>({
+        method: 'GET',
+        url: '/user/:username',
+        schema: {
+            tags: ['Party'],
+            description: "Get all parties of a certain user which you are invited at"
+        },
+        preValidation: async (req, res) => {
+            fastify.verifyJwt(req, res);
+        },
+        handler: async (req, res) => {
+            const user = await connection.getRepository(User).findOne({ username: req.params.username });
+            if(!user) return res.status(400).send("User not found");
+
+            const parties = await connection.getRepository(Party).find({ creator: user });
+
+            if(user.id === req.user.id){
+                return res.status(200).send({ parties: parties });
+            }
+            else {
+                const invitedParties: Party[] = [];
+                for(const party of parties){
+                    const invitations = await connection.getRepository(Invitation).find({ party: party });
+                    for(const invitation of invitations){
+                        if(invitation.user.id === req.user.id){
+                            invitedParties.push(party);
+                            break;
+                        }
+                    }
+                }
+                return res.status(200).send({ parties: invitedParties });
+            }
         }
     });
 
@@ -55,12 +107,35 @@ export const partyRoutes = async (fastify: FastifyInstance) => {
         handler: async (req, res) => {
             const party = await connection.getRepository(Party).findOne({ id: req.params.id });
             if(!party) return res.status(400).send("Sorry this party doesn't exist");
+
+            const user = await connection.getRepository(User).findOne({ id: req.user.id });
+            if(!user) return res.status(400).send("Unexpected");
+
+            const myParty = await connection.getRepository(Party).findOne({ id: req.params.id, creator: user });
+            if(myParty) return res.status(200).send(party);
+
+            const invitation = await connection.getRepository(Invitation).findOne({ party: party, user: user });
+            if(!invitation) return res.status(400).send("Sorry you are not invited to this party");
+
             res.status(200).send(party);
         }
     });
 
-    fastify.route<{ Params : { id: string }}>({
-        method: 'PUT',
+    fastify.route<{
+        Params : {
+            id: string
+        },
+        Body: {
+            name?: string,
+            location?: string,
+            date?: Date,
+            description?: string,
+            minimumAge?: number,
+            maximumAge?: number,
+
+        }
+    }>({
+        method: 'PATCH',
         url: '/:id',
         schema: {
             tags: ['Party']
@@ -69,9 +144,48 @@ export const partyRoutes = async (fastify: FastifyInstance) => {
             fastify.verifyJwt(req, res);
         },
         handler: async (req, res) => {
-            const id = parseInt(req.params.id);
-            if(isNaN(id)) return res.status(400).send();
-            return res.send(req.params.id);
+            const party = await connection.getRepository(Party).findOne({ id: req.params.id });
+            if(!party) return res.status(400).send("Party not found");
+
+            const user = await connection.getRepository(User).findOne({ id: req.user.id });
+            if(!user) return res.status(400).send("Unexpected");
+            const myParty = await connection.getRepository(Party).findOne({ id: req.params.id, creator: user });
+            if(!myParty) return res.status(400).send("You are not authorized to edit this party");
+
+            if(req.body.name) myParty.name = req.body.name;
+            if(req.body.location) myParty.location = req.body.location;
+            if(req.body.date) myParty.date = req.body.date;
+            if(req.body.description) myParty.description = req.body.description;
+            if(req.body.minimumAge) myParty.minimumAge = req.body.minimumAge;
+            if(req.body.maximumAge) myParty.maximumAge = req.body.maximumAge;
+
+            await connection.getRepository(Party).save(myParty);
+
+            res.status(200).send(myParty);
+        }
+    });
+
+    fastify.route<{ Params: { id: string }}>({
+        method: 'DELETE',
+        url: '/:id',
+        schema: {
+            tags: ['Party']
+        },
+        preValidation: async (req, res) => {
+            fastify.verifyJwt(req, res);
+        },
+        handler: async (req, res) => {
+            const party = await connection.getRepository(Party).findOne({ id: req.params.id });
+            if(!party) return res.status(400).send("Party not found");
+
+            const user = await connection.getRepository(User).findOne({ id: req.user.id });
+            if(!user) return res.status(400).send("Unexpected");
+            const myParty = await connection.getRepository(Party).findOne({ id: req.params.id, creator: user });
+            if(!myParty) return res.status(400).send("You are not authorized to edit this party");
+
+            await connection.getRepository(Party).remove(myParty);
+
+            res.status(200).send("Party successfully deleted");
         }
     });
 }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { connection } from '../lib/connection';
-import { User } from '../entities/User';
+import { RelationshipStatus, User } from '../entities/User';
 import { hashPassword } from '../lib/passwordEncryption';
 import * as UserSchema from '../schemas/user.json';
 
@@ -31,6 +31,91 @@ export const userRoutes = async (fastify: FastifyInstance) => {
         }
     });
 
+    fastify.route({
+        method: 'GET',
+        url: '/',
+        schema: {
+            tags: ['User']
+        },
+        preValidation: async (req, res) => {
+            fastify.verifyJwt(req, res);
+        },
+        handler: async (req, res) => {
+            const users = await connection.getRepository(User).find();
+            const usernames = [];
+
+            for(const user of users){
+                usernames.push(user.username);
+            }
+
+            res.status(200).send({ usernames: usernames });
+        }
+    });
+
+    fastify.route<{
+        Body: {
+            firstname?: string,
+            lastname?: string, 
+            email?: string,
+            birthdate?: Date,
+            relationshipStatus?: RelationshipStatus
+        },
+        Params: {
+            username: string
+        }
+    }>({
+        method: 'PATCH',
+        url: '/:username',
+        schema: {
+            tags: ['User']
+        },
+        preValidation: async (req, res) => {
+            fastify.verifyJwt(req, res);
+        },
+        handler: async (req, res) => {
+            const user = await connection.getRepository(User).findOne({ username: req.params.username });
+            if(!user) return res.send(400).send("User not found, check username");
+
+            if(user.id !== req.user.id) return res.status(400).send("You are not authorized to modify this user");
+            
+            if(req.body.firstname) user.firstname = req.body.firstname;
+            if(req.body.lastname) user.lastname = req.body.lastname;
+            if(req.body.email) user.email = req.body.email;
+            if(req.body.birthdate) user.birthdate = req.body.birthdate;
+            if(req.body.relationshipStatus) user.relationshipStatus = req.body.relationshipStatus;
+
+            await connection.getRepository(User).save(user);
+
+            res.status(200).send({ user: {
+                firstname: user.firstname,
+                lastname: user.lastname,
+                email: user.email,
+                birthdate: user.birthdate,
+                relationshipStatus: user.relationshipStatus
+            }});
+        }
+    });
+
+    fastify.route<{ Params: { username: string }}>({
+        method: 'DELETE',
+        url: '/:username',
+        schema: {
+            tags: ['User']
+        },
+        preValidation: async (req, res) => {
+            fastify.verifyJwt(req, res);
+        },
+        handler: async (req, res) => {
+            const user = await connection.getRepository(User).findOne({ username: req.params.username });
+            if(!user) return res.status(400).send("User not found");
+            if(user.id !== req.user.id) return res.status(400).send("You are not authorized to delete this user");
+
+            await connection.getRepository(User).delete(user);
+
+            res.status(200).send("User successfully deleted");
+        }
+    });
+
     fastify.route<{ Params: { username: string }}>({
         method: 'GET',
         url: '/:username',
@@ -42,11 +127,33 @@ export const userRoutes = async (fastify: FastifyInstance) => {
         },
         handler: async (req, res) => {
             const user = await connection.getRepository(User).findOne({ username: req.params.username });
-            if(user){
-                res.status(200).send({ user: { id: user.id, username: user.username }});
+            if(!user) return res.status(400).send("Sorry this user doesn't exist");
+
+            if(user.id === req.user.id){
+                return res.status(200).send({
+                    user: {
+                        id: user.id,
+                        username: user.username,
+                        firstname: user.firstname,
+                        lastname: user.lastname,
+                        email: user.email,
+                        parties: user.parties,
+                        receivedInvitations: user.receivedInvitations,
+                        sentInvitations: user.sentInvitations,
+                        birthdate: user.birthdate,
+                        relationshipStatus: user.relationshipStatus
+                    }
+                });
             }
             else {
-                res.status(200).send("Sorry this user doesn't exist");
+                return res.status(200).send({
+                    user: {
+                        id: user.id,
+                        username: user.username,
+                        birthdate: user.birthdate,
+                        relationshipStatus: user.relationshipStatus
+                    }
+                });
             }
         }
     });
